633070a3dac47f3f3a2f066f9c13ef9b3658d7e8
Andrés López <saurioandres456@gmail.com>
2020-07-19 22:02:43 -0500

    Add Custom UITextField for Login Screen

--- LanguagesApp/ViewController.swift
+++ LanguagesApp/ViewController.swift
@@ -10,12 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = ""
+        setTitle()
+        setTextFieldProperties()
+        
     }
 
-    //Hi
+    func setTitle()
+    {
+        let titleName = "LanguagesApp"
+        var index = 0.0
+        
+        for letter in titleName
+        {
+            Timer.scheduledTimer(withTimeInterval: 0.09 * index, repeats: false, block: {(timer) in
+                self.titleLabel.text?.append(letter)
+            })
+            
+            index += 1
+        }
+    }
+
+    func setTextFieldProperties()
+    {
+        userName.borderStyle = .none
+        userPassword.borderStyle = .none
+        
+        userName.layoutIfNeeded()
+        userPassword.layoutIfNeeded()
+        
+        let borderName = CALayer()
+        let width = CGFloat(14.0)
+        
+        borderName.borderColor = UIColor.darkGray.cgColor
+        
+        borderName.frame = CGRect(x: 0, y: userName.frame.size.height - width, width: userName.frame.size.width, height: userName.frame.size.height)
+        
+        borderName.borderWidth = width
+        
+        userName.layer.addSublayer(borderName)
+        userName.layer.masksToBounds = true
+        
+        let borderPassword = CALayer()
+        let width2 = CGFloat(15.8)
+        
+        borderPassword.borderColor = UIColor.darkGray.cgColor
+        
+        borderPassword.frame = CGRect(x: 0, y: userPassword.frame.size.height - width2, width: userPassword.frame.size.width, height: userPassword.frame.size.height)
+        
+        borderPassword.borderWidth = width2
+        
+        userPassword.layer.addSublayer(borderPassword)
+        userPassword.layer.masksToBounds = true
+    }
 
 }
 
